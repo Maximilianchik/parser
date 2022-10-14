d551c7bdc8f2851bf18f43106db3538cc7d8e114
user <max.otto.shtirlits59@gmail.com>
2022-10-14 13:01:46 +0300

    kjlkmpio

--- main.py
+++ main.py
@@ -6,32 +6,18 @@ import time
 import json
 
 
-def get_url():
+def get_books():
     cur_time = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
+    soup, header = get_html_text()
+    genre = soup.find('h1', class_='genre-name').text
 
-    with open(f'labitint_{cur_time}.csv', 'w', encoding='utf-8') as file:
-        writer = csv.writer(file)
+    save_csv(cur_time, genre)
 
-        writer.writerow(
-            (
-                'Название книги',
-                'Автор книги',
-                'Издательство',
-                'Цена',
-                'Скидка на книгу'
-            )
-        )
+    soup, header = get_html_text()
 
-    url = 'https://www.labirint.ru/genres/2308/'
-    header = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.2.1495 Yowser/2.5 Safari/537.36'
-    }
-    responce = requests.get(url=url, headers=header).text
-    soup = BeautifulSoup(responce, 'html.parser')
     page_count = int(soup.find('div', class_='pagination-numbers').find_all('a')[-1].text)
-
     books_data = []
+
 
     for page in range(1, page_count + 1):
         url = f'https://www.labirint.ru/genres/2308/?page={page}'
@@ -60,17 +46,11 @@ def get_url():
                 book_publishment = 'Нет издательства'
 
             try:
-                book_cell = book.find('', class_='').text.strip()
+                book_cell = book.find('span', class_='card-label__text card-label__text_turned').text.strip()
             except:
                 book_cell = 'Нет скидки'
 
-            '''
-            print('#################################################################')
-            print('Название-'+str(book_title))
-            print('Автор-'+str(book_author))
-            print('Цена-'+str(book_price))
-            print('Издательство-'+str(book_publishment))
-            '''
+
             books_data.append(
                 {
                     'book_title': book_title,
@@ -80,7 +60,7 @@ def get_url():
                     'book_cell': book_cell
                 }
             )
-            with open(f'labitint_{cur_time}.csv', 'a', encoding='utf-8') as file:
+            with open(f'labitint_{cur_time}_{genre}.csv', 'a', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerow(
                     (
@@ -91,12 +71,43 @@ def get_url():
                         book_cell
                     )
                 )
-
-        print(str(page) + '/16-страница')
+        print(str(page) + f'/{page_count}-страница')
         time.sleep(1)
-    with open(f'labirint_{cur_time}.json', 'w', encoding='utf-8') as file:
+    save_json(books_data, cur_time, genre)
+
+
+
+
+def get_html_text():
+    url = 'https://www.labirint.ru/genres/2308/'
+    header = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.2.1495 Yowser/2.5 Safari/537.36'
+    }
+    responce = requests.get(url=url, headers=header).text
+    soup = BeautifulSoup(responce, 'html.parser')
+    return soup, header
+
+
+def save_json(books_data, cur_time, genre):
+    with open(f'labirint_{cur_time}_{genre}.json', 'w', encoding='utf-8') as file:
         json.dump(books_data, file, indent=4, ensure_ascii=False)
 
 
+def save_csv(cur_time, genre):
+    with open(f'labitint_{cur_time}_{genre}.csv', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(
+            (
+                'Название книги',
+                'Автор книги',
+                'Издательство',
+                'Цена',
+                'Скидка на книгу',
+                'Наличие'
+            )
+        )
+
 if __name__ == '__main__':
-    get_url()
+    get_books()
